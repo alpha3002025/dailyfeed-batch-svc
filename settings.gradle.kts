3f7fdc941b58d4eb4895e6e976c66d771f4b7f97
alpha3002025 <alpha300uk@gmail.com>
2025-10-19 19:42:50 +0900

    feat: 카프카 Publisher,Listener 의 통신실패 처리기능 개발

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,9 @@
 rootProject.name = "dailyfeed-batch-svc"
 
 include(
+    "dailyfeed-code",
+    "dailyfeed-redis-support",
+    "dailyfeed-kafka-support",
+    "dailyfeed-pvc-support",
     "dailyfeed-batch",
 )
