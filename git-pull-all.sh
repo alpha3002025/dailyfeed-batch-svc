adba2387cc762052c6ff3e7adc0c5367dc4014f1
alpha3002025 <alpha300uk@gmail.com>
2025-10-29 08:40:42 +0900

    chore: modified shell script

---
+++ git-pull-all.sh
@@ -0,0 +1,39 @@
+echo "dailyfeed-code"
+cd dailyfeed-code
+git pull origin main
+cd ..
+echo ""
+
+echo "dailyfeed-batch"
+cd dailyfeed-batch
+git pull origin main
+cd ..
+echo ""
+
+
+echo "dailyfeed-deadletter-support"
+cd dailyfeed-deadletter-support
+git pull origin main
+cd ..
+echo ""
+
+
+echo "dailyfeed-kafka-support"
+cd dailyfeed-kafka-support
+git pull origin main
+cd ..
+echo ""
+
+
+echo "dailyfeed-pvc-support"
+cd dailyfeed-pvc-support
+git pull origin main
+cd ..
+echo ""
+
+
+echo "dailyfeed-redis-support"
+cd dailyfeed-redis-support
+git pull origin main
+cd ..
+echo ""
